4df810510c8c40f57cea38af20e32abb28077d97
hunghx <120539776+hunghx@users.noreply.github.com>
2024-12-24 17:08:16 +0700

    Add files via upload

--- sesion12B/btvn.cpp
+++ sesion12B/btvn.cpp
@@ -36,28 +36,31 @@ int main(){
 
 	// cot có chi so = 0; có m phan tu
 	
-		int k = 0;
-		int col = 0;
-		for(int i = 0; i < m*n-1;i++){
+	
+	
+		for(int i = 0; i < m*n-1 ;i++){
+			int col = 0;
+			int row = 0;
 			for(int j = 0; j < m*n-1-i ; j++){
-				if(k%4 != 3 ){
-					if(array[k][col] > array[k+1][col]){
-						int temp = array[k][col];
-						array[k][col] = array[k+1][col];
-						array[k+1][col] = temp;
+				if(row%m != m-1 ){
+//					printf("\n%d",j);
+					if(array[row][col] > array[row+1][col]){
+						int temp = array[row][col];
+						array[row][col] = array[row+1][col];
+						array[row+1][col] = temp;
 					}
+					row++;
 				}else{
-					if(array[k][m-1] > array[0][col+1]){
-						int temp = array[k][m-1];
-						array[k][m-1] = array[0][col+1];
+					if(array[row][col] > array[0][col+1]){
+						int temp = array[row][col];
+						array[row][col] = array[0][col+1];
 						array[0][col+1] = temp;
 					}
-					k = 0;
+					row = 0;
 					col++;
-					continue;
-				}
-				k++;
+				}	
 			}
+			
 		}
 
 // duong cheo chinh : i == j
